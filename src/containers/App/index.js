b9bdf1224ae356869691bd65630d533ea58603a4
abhijeetskadam <abhijeetskadam@gmail.com>
2019-05-12 18:36:23 +0200

    App restructuring

---
+++ src/containers/App/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./app-styles.css";
+import HomePage from "../HomePage";
+
+function App() {
+  return (
+    <React.Fragment>
+      {/* Routing should be done here */}
+      <HomePage />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/containers/HomePage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+function HomePage() {
+  return (
+    <div className="App">
+      <header className="App-header">Demo D3 app</header>
+    </div>
+  );
+}
+
+export default HomePage;
