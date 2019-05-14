54e771b41ae0de58929e03cc4a1cf4030fd59429
abhijeetskadam <abhijeetskadam@gmail.com>
2019-05-14 13:50:05 +0200

    Added LineChart to Homepage

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import "./styles.css";
+import LineChart from "../../components/LineChart";
+import ErrorOrLoader from "../../components/ErrorOrLoader";
+import request from "../../utils/request";
 
 function HomePage() {
+  const [data, setData] = useState([]);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    request("/data")
+      .then(result => setData(result.values))
+      .catch(() => setIsError(true));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">Demo D3 app</header>
-    </div>
+    <Fragment>
+      <header className="app__header">D3 Demo App</header>
+      <div className="app">
+        {data.length > 0 ? (
+          <LineChart
+            height={500}
+            width={800}
+            margin={20}
+            data={data}
+            gridLines={true}
+          />
+        ) : (
+          <ErrorOrLoader isError={isError} />
+        )}
+      </div>
+    </Fragment>
   );
 }
 
